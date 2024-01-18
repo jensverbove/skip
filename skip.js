4cb804a7b6f1af4374e1d94e90d2589feb9896a1
Jens Verboven <Jens.Verboven-CIC@ibm.com>
2024-01-18 17:02:50 +0100

    fix skip coding exercises

--- skip.js
+++ skip.js
@@ -2,74 +2,89 @@
 const loadTime = 4000;
 
 let clickAndSkip = async (link) => {
-  link.click();
 
-  await new Promise((resolve) => setTimeout(resolve, loadTime));
+    link.click();
 
-  quizTime = link
-    .querySelector('[data-purpose="item-title"]')
-    .innerText.toLowerCase()
-    .includes("quiz");
+    let noCodingPlease = !link
+        .querySelector('[data-purpose="item-title"]')
+        .innerText.toLowerCase()
+        .includes("coding exercise");
 
-  if (quizTime) {
-    console.log("Quiz time!");
+    if (noCodingPlease) {
 
-    continueButton = document.querySelector('[data-purpose="start-or-resume-quiz"]')
-    if(continueButton != null){
-      continueButton.click();
-    }
+        await new Promise((resolve) => setTimeout(resolve, loadTime));
 
-    await new Promise((resolve) => setTimeout(resolve, 500));
+        let quizTime = link
+            .querySelector('[data-purpose="item-title"]')
+            .innerText.toLowerCase()
+            .includes("quiz");
 
-    while (document.querySelector('[data-purpose="go-to-next"]') == null) {
-      answers = document.getElementsByClassName(
-        "mc-quiz-question--answer--eCdL3"
-      );
-      nextButton = document.querySelector(
-        '[data-purpose="next-question-button"]'
-      );
-      for (let answer of Array.from(answers)) {
-        answer.getElementsByTagName("input")[0].click();
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        nextButton.click();
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        correct = nextButton.innerText.toLowerCase().includes("next");
+        if (quizTime) {
+            console.log("Quiz time!");
 
-        if (correct) {
-          console.log("Correct!");
-          nextButton.click();
-          break;
+            continueButton = document.querySelector(
+                '[data-purpose="start-or-resume-quiz"]'
+            );
+            if (continueButton != null) {
+                continueButton.click();
+            }
+
+            await new Promise((resolve) => setTimeout(resolve, 500));
+
+            while (document.querySelector('[data-purpose="go-to-next"]') == null) {
+                answers = document.getElementsByClassName(
+                    "mc-quiz-question--answer--eCdL3"
+                );
+                nextButton = document.querySelector(
+                    '[data-purpose="next-question-button"]'
+                );
+                for (let answer of Array.from(answers)) {
+                    answer.getElementsByTagName("input")[0].click();
+                    await new Promise((resolve) => setTimeout(resolve, 500));
+                    nextButton.click();
+                    await new Promise((resolve) => setTimeout(resolve, 500));
+                    correct = nextButton.innerText.toLowerCase().includes("next");
+
+                    if (correct) {
+                        console.log("Correct!");
+                        nextButton.click();
+                        break;
+                    }
+                }
+            }
+            document.querySelector('[data-purpose="go-to-next"]').click();
         }
-      }
+
+        video = document.getElementsByTagName("video")[0];
+        if (video != undefined) {
+            video.currentTime += 10000;
+            console.log("Skipped!");
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        
+    } else {
+        console.log("Do it youself! (or Google it)")
     }
-    document.querySelector('[data-purpose="go-to-next"]').click();
-  }
 
-  video = document.getElementsByTagName("video")[0];
-  if (video != undefined) {
-    video.currentTime += 10000;
-    console.log("Skipped!");
-  }
-
-  await new Promise((resolve) => setTimeout(resolve, 1000));
 };
 
 // Run this function to skip all the lectures in the sections that are currently opened.
 let skip = async () => {
-  elements = document.getElementsByClassName("item-link");
-  links = Array.from(elements).filter(function (element) {
-    return element.getAttribute("data-purpose").startsWith("curriculum-item");
-  });
-  for (let link of Array.from(links)) {
-    await clickAndSkip(link);
-  }
+    elements = document.getElementsByClassName("item-link");
+    links = Array.from(elements).filter(function (element) {
+        return element.getAttribute("data-purpose").startsWith("curriculum-item");
+    });
+    for (let link of Array.from(links)) {
+        await clickAndSkip(link);
+    }
 };
 
 // Run this function to open/close all the sections in Udemy.
 let openAllSections = () => {
-  buttons = document.getElementsByClassName("ud-accordion-panel-toggler");
+    buttons = document.getElementsByClassName("ud-accordion-panel-toggler");
 
-  for (let button of buttons) {
-    button.click();
-  }
+    for (let button of buttons) {
+        button.click();
+    }
 };
